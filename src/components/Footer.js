7db02e7146f2753e503fb2c7bfed2c1f1d1281ab
Yao Bin Then <alvinthen890703@gmail.com>
2018-01-10 18:20:59 +0800

    💬 Update social media handles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,14 +14,14 @@ export default () => (
               </a>
             </li>
             <li>
-              <a href="https://facebook.com/yaobin89" target="_blank" rel="noopener">
+              <a href="https://facebook.com/yaobin.me" target="_blank" rel="noopener">
                 <span className="screen-reader">Open Facebook account in new tab</span><svg className="icon" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
                   <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                 </svg>
               </a>
             </li>
             <li>
-              <a href="https://instagram.com/yaobin89" target="_blank" rel="noopener">
+              <a href="https://instagram.com/yaobin.me" target="_blank" rel="noopener">
                 <span className="screen-reader">Open Instagram account in new tab</span><svg className="icon" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
                   <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
